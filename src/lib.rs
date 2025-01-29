f8e6e2081b4feb635d46157b443b69c6ab5e68de
Kauã Klassmann <kaua.klassmann661@gmail.com>
2025-01-29 15:03:58 -0300

    0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -2,41 +2,5 @@
 //! The library is designed to simplify user input handling, offering an easy way to display prompts
 //! and capture input without needing to handle `Result` or `Option` explicitly.
 
-use std::io::{self, Write};
-
-/// A trait that defines prompt functionality for obtaining user input as a `String`.
-/// 
-/// This trait allows any type that implements `StringPromptable` to provide a prompt
-/// for user input, making it easier to capture data of type `String`.
-/// 
-/// # Methods
-/// 
-/// - `prompt`: Displays a prompt message to the user and returns the input as the type implementing the trait.
-pub trait StringPromptable {
-    /// Displays a prompt message to the user and returns the input as the type that implements the trait.
-    ///
-    /// # Parameters
-    ///
-    /// - `output`: The message that will be shown to the user.
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    /// let name: String = String::prompt("What is your name? ");
-    /// ```
-    fn prompt(output: &str) -> Self;
-}
-
-impl StringPromptable for String {
-    fn prompt(output: &str) -> Self {
-        print!("{}", output);
-        io::stdout().flush().unwrap();
-    
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read the prompt");
-
-        input.pop();
-
-        input
-    }
-}
+pub mod prelude;
+mod primitive;
