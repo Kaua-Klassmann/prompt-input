2aa6d983fe9227cfa971459d7660566cca922cd7
Kauã Klassmann <kaua.klassmann661@gmail.com>
2025-02-23 19:31:44 -0300

    0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! A simple library that provides functionality to prompt for user input.
 //! The library is designed to simplify user input handling, offering an easy way to display prompts
-//! and capture input without needing to handle `Result` or `Option` explicitly.
+//! and capture input.
 
 pub mod prelude;
 mod primitive;
--- src/primitive.rs
+++ src/primitive.rs
@@ -1,38 +1,36 @@
 use std::io::{self, Write};
 
-/// A trait that defines prompt functionality for obtaining user input as a `String`.
-/// 
-/// This trait allows any type that implements `StringPromptable` to provide a prompt
-/// for user input, making it easier to capture data of type `String`.
-/// 
-/// # Methods
-/// 
-/// - `prompt`: Displays a prompt message to the user and returns the input as the type implementing the trait.
-pub trait StringPromptable {
-    /// Displays a prompt message to the user and returns the input as the type that implements the trait.
-    ///
-    /// # Parameters
-    ///
-    /// - `output`: The message that will be shown to the user.
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    /// let name: String = String::prompt("What is your name? ");
-    /// ```
+pub trait Promptable {
     fn prompt(output: &str) -> Self;
 }
 
-impl StringPromptable for String {
+pub trait PromptableWithOption
+where
+    Self: Sized,
+{
+    fn prompt(output: &str) -> Option<Self>;
+}
+
+impl Promptable for String {
     fn prompt(output: &str) -> Self {
         print!("{}", output);
         io::stdout().flush().unwrap();
-    
+
         let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read the prompt");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read the prompt");
 
         input.pop();
 
         input
+    }
+}
+
+impl PromptableWithOption for i32 {
+    fn prompt(output: &str) -> Option<Self> {
+        let input = String::prompt(output);
+
+        input.parse().ok()
     }
 }
